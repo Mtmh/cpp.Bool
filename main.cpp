9085dff4bd04d7b83cc71d7cd059696f07ab1e4d
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-27 11:51:30 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool m = 0; // 0 false // 1 true
+    int x = 56;
+    cout << m << endl;
+    cout << endl;
+    cout << x << endl;
+    cout << endl;
+    cout << true << endl;
+    cout << endl;
+    cout << false << endl;
+    cout << endl;
+    cout << std::boolalpha; // Mirar boolalpha
+    cout << endl;
+    cout << false << endl;
+    cout << endl;
+    cout << true << endl;
+    cout << endl;
+    cout <<  0 << endl;  // No los
+    cout << endl;
+    cout << 1 << endl; // Confunde
+    cout << endl;
+
+
+    return 0;
+}
